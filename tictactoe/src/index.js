5886af141e41e64739dc35ea2993f3dfecb4fcf6
cliftonbartholomew <cliftonbartholomew@gmail.com>
2022-10-08 13:52:57 +0200

    implemented tictactoe in react

---
+++ tictactoe/src/index.js
@@ -0,0 +1,6 @@
+import ReactDOM from "react-dom/client";
+import Game from "./components/Game";
+import "./index.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Game />);
